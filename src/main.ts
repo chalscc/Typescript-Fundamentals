42a548e88928e0c4138fe9a1b5553eeb15d8f3d1
chalscc <carlescatalancalabuig@gmail.com>
2023-11-16 16:36:50 +0100

    Basic types and general concepts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+
 import { setupCounter } from './counter.ts'
+import './topics/01-basic-types'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola mundo
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,16 @@
+const name: string = 'Carles';
+let hpPoints: number | 'Full' = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'Full';
+
+console.log(
+    {
+        name,
+        hpPoints,
+        isAlive
+    }
+)
+
+
+export {};
