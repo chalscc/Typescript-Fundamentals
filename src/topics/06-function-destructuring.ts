925ea52ca0760f37fad97919e7056926d9d907ab
chalscc <carlescatalancalabuig@gmail.com>
2023-11-16 18:41:59 +0100

    22-23. Destructuring exercise

---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,43 @@
+
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Nokia A1',
+    price: 150.0,
+}
+
+const tablet: Product = {
+    description: 'iPad Air',
+    price: 250.0,
+}
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+interface TaxCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+function taxCalculation(options: TaxCalculationOptions): [number, number] {
+
+    let total = 0;
+    const { tax, products } = options;
+
+    products.forEach(({price}) => {
+        total += price;
+    })
+
+    return [total, total * tax];
+}
+
+const [total, taxTotal] = taxCalculation({tax, products: shoppingCart});
+
+console.log('Total: ', total);
+console.log('Tax: ', taxTotal);
+
+
+export {};
