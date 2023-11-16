5529f1a18b70910796a2d90c72f730374935aca4
chalscc <carlescatalancalabuig@gmail.com>
2023-11-16 16:50:43 +0100

    Objects, arrays and interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import './style.css'
 
-import { setupCounter } from './counter.ts'
-import './topics/01-basic-types'
+// import './topics/01-basic-types'
+import './topics/02-object-interface';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola mundo
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
