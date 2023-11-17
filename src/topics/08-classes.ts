0d89d2161b864735cc0f35bcfff8c23b6058b734
chalscc <carlescatalancalabuig@gmail.com>
2023-11-17 13:51:02 +0100

    27. Extending a class

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -5,12 +5,23 @@ export class Person {
 
     constructor( 
         public name: string, 
-        private address: string
+        private address: string = 'No address'
         ) {}
 
 }
 
+export class Hero extends Person { 
 
-const ironman = new Person('Carles', 'Olleria');
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+    ) {
+        super(realName, 'New York');
+    }
+}
+
+
+const ironman = new Hero('Ironman', 45, 'Tony Stark');
 
 console.log(ironman);
