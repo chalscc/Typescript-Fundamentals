f434c99f5a549c5842612cfc2ee64de02d6c2939
chalscc <carlescatalancalabuig@gmail.com>
2024-06-22 15:36:42 +0200

    Installing testing to the project

---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+// Codigo que permite a versiones anteriores de node (anteriores a la 18) usar el fetch
+// -D por que es una dev dependency
+// npm install -D whatwg-fetch
+import 'whatwg-fetch'
---
+++ test/exercises/01-array.test.js
@@ -0,0 +1,11 @@
+describe('Test sobre <CounterApp/>', () => { 
+
+    const initialValue = 100;
+
+    test('Test 1', () => { 
+        
+        expect(initialValue).toBe(100);
+
+     })
+
+ })
